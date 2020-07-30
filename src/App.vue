85da7e0f11af02a966ccc162a4c16a08528df406
Barnabas Asha <barnabee58@gmail.com>
2020-07-30 12:15:22 +0100

    Intialized myName

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <myName 
+    :name ="name"/>
+    
+  </div>
+</template>
+
+<script>
+
+import myName from './components/myName.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    myName
+  },
+  data () {
+    return {
+      name: 'Barnabas Asha'
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/myName.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="my-name">
+    <h1>{{name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String
+    }
+}
+</script>
+
+<style scoped>
+    .my-name {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100vh;
+        background: #f1f1f2;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+
+    .my-name h1 {
+        font-size: 3rem;
+        font-family: 'Lucida sans', sans-serif;
+        color: #0f095e;
+    }
+</style>
